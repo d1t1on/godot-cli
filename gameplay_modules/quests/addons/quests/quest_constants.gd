8e3aa62f9a297eca11619498ff78398da270df31
Ubuntu <ubuntu@localhost.localdomain>
2026-06-11 15:47:50 +0800

    Add quest event constants

--- gameplay_modules/quests/addons/quests/quest_constants.gd
+++ gameplay_modules/quests/addons/quests/quest_constants.gd
@@ -16,3 +16,11 @@ const OBJECTIVE_COMPLETED: String = "completed"
 
 const POLICY_ALL: String = "all"
 const POLICY_ANY: String = "any"
+
+const EVENT_QUEST_STARTED: String = "quest_started"
+const EVENT_QUEST_COMPLETED: String = "quest_completed"
+const EVENT_QUEST_FAILED: String = "quest_failed"
+const EVENT_QUEST_STATUS_CHANGED: String = "quest_status_changed"
+const EVENT_OBJECTIVE_PROGRESS_CHANGED: String = "objective_progress_changed"
+const EVENT_OBJECTIVE_COMPLETED: String = "objective_completed"
+const EVENT_QUESTS_CHANGED: String = "quests_changed"
